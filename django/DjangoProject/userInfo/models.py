3d53b6796ade2a4f4b62911d6130e2c68cfa6874
santhosh karthi <santhoshkarthisk005@gmail.com>
2025-02-27 09:09:45 +0530

    changes for field updation and add new readme file

--- django/DjangoProject/userInfo/models.py
+++ django/DjangoProject/userInfo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class UserInfo(models.Model):
-   name = models.CharField(max_length=20, null=True)
+   name = models.CharField(max_length=35, null=True)
    age = models.IntegerField(default=0) 
    gender = models.CharField(max_length=10, null=True)
    phone = models.CharField(max_length=20, null=True)
